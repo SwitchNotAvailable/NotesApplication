fde596e9474fde003d63f3e25e68885a4a2f4a0a
SwitchNotAvailable <jeffreykelechi57@gmail.com>
2023-12-01 19:54:49 +0000

    Second commit (having issues with JAR version)

--- app/src/main/java/com/griffith/notesapplication/MainActivity.kt
+++ app/src/main/java/com/griffith/notesapplication/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.griffith.notesapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.SearchView
+import androidx.cardview.widget.CardView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,13 +19,113 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.griffith.notesapplication.Adapter.NotesAdapter
+import com.griffith.notesapplication.Database.NoteDatabase
+import com.griffith.notesapplication.Models.Note
+import com.griffith.notesapplication.Models.NoteViewModel
+import com.griffith.notesapplication.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), NotesAdapter.NotesitemclickListener, PopupMenu.OnMenuItemClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel : NoteViewModel
+    lateinit var  adapter: NotesAdapter
+    lateinit var selectedNote : Note
+
+    private val updateNote = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val note = result.data?.getSerializableExtra("note") as? Note
+            if (note != null) {
+                viewModel.updateNote(note)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        initUI()
+
+        viewModel = ViewModelProvider (this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+
+        viewModel.allnotes.observe(this) { list ->
+
+            list?.let {
+                adapter.updateList(list)
+            }
         }
+
+        database = NoteDatabase.getDatabase(this)
+    }
+
+    private fun initUI() {
+
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                val note = result.data?.getSerializableExtra("note") as? Note
+                if (note != null) {
+                    viewModel.insertNote(note)
+                }
+            }
+        }
+
+        binding.fbAddNote.setOnClickListener{
+            val intent = Intent(this, AddNote::class.java)
+            getContent.launch(intent)
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    adapter.filerList(newText)
+                }
+                return true
+            }
+        })
+    }
+
+    override fun onitemClicked(note: Note) {
+
+        val intent = Intent(this@MainActivity,AddNote::class.java)
+        intent.putExtra("current_note", note)
+        updateNote.launch(intent)
+    }
+
+    override fun onLongItemClicked(note: Note, cardView: CardView) {
+        selectedNote = note
+        popUpDisplay(cardView)
+
+    }
+
+    private fun popUpDisplay(cardView: CardView) {
+        val popup = PopupMenu(this,cardView)
+        popup.setOnMenuItemClickListener(this@MainActivity)
+        popup.inflate(R.menu.pop_up_menu)
+        popup.show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.delete_note) {
+            viewModel.deleteNote(selectedNote)
+            return true
+        }
+        return false
     }
 }
 
---
+++ app/src/main/java/com/griffith/notesapplication/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.griffith.notesapplication.utilities
+
+const val DATABASE_NAME = "note_database"
